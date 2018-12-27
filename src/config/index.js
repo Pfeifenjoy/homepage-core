834cd49c0880696bd8dda4478278321c53385ffa
Arwed Mett <arwed.mett@googlemail.com>
2018-12-27 21:44:11 +0100

    changed tilde to absolute path

--- src/config/index.js
+++ src/config/index.js
@@ -15,6 +15,8 @@ import { Config as Database } from "./database"
 import { load as load_db, description as db_description } from "./database"
 import { load as load_notifiers, description as notifier_description } from "./notifier"
 import type { Notifier } from "./notifier"
+import { homedir } from "os"
+import { join } from "path"
 
 export class Config {
 	db: Database
@@ -37,9 +39,9 @@ export const description = object({
 export const load = async (default_path: ?string): Promise<Config> => {
 	const search_paths = default_path === undefined || default_path === null ? [
 		"./homepagerc.json",
-		"~/.homepagerc.json",
-		"~/.config/homepage/homepagerc.json",
-		"~/.config/homepage/.homepagerc.json",
+		join(homedir(), ".homepagerc.json"),
+		join(homedir(), ".config/homepage/homepagerc.json"),
+		join(homedir(), ".config/homepage/.homepagerc.json"),
 		"/etc/homepage/homepagerc.json",
 		"/etc/homepage/.homepagerc.json"
 	] : [ default_path ]
@@ -64,6 +66,7 @@ export const load = async (default_path: ?string): Promise<Config> => {
 	const data = fallback(get_object)({ })(sanitize(description)(raw_data))
 
 	const base_path = fallback(get_string)("~/.homepage")(data.base_path)
+		.replace("~", homedir())
 	const db_settings = fallback(get_object)({ })(data.db)
 	const db = await load_db(base_path)(db_settings)
 	const notifiers_settings = fallback(get_array)([ ])(data.notifiers)
